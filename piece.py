3d0d516bf3b868efd621688e9712661814cc5ec0
anhkhoa2174 <tkhoa4040@gmail.com>
2024-11-30 01:34:00 +0700

    Update Metainfo class

--- piece.py
+++ piece.py
@@ -9,13 +9,13 @@ from threading import Thread
 import sys
 import json
 
-class piece:
-    def __init__(self, size, hash, parent, path):
-        #self.id
-        self.pieceSize =size
-        self.hashcode  = hash
-        self.pathParent = parent
-        self.path  = path
+# class piece:
+#     def __init__(self, size, hash, parent, path):
+#         #self.id
+#         self.pieceSize =size
+#         self.hashcode  = hash
+#         self.pathParent = parent
+#         self.path  = path
         
         
         
